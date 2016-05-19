1c231d18d9919100ad9d4fa7cb59ebbd439fe520
Sai Pratap Koppolu <saipratapkoppolu@Sais-MacBook-Pro.local>
2016-05-19 12:12:57 +0800

    Added files for Application

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,41 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var first = 15
+
+var second = 5
+
+var sum = first+second
+
+var product = first*second
+
+var subtraction = first-second
+
+var division = first/second
+
+var one = 1, two = 2, three = 3
+
+let four = 4, five = 5, six = 6
+
+var seven:Double
+
+var eight:Int = 423
+
+var str = "Hello"
+
+var str2 = "World"
+
+var str3 = str+" "+str2
+
+
+
+
+
+
+
+
+
+
+
+
--- Red&Blue/Red&Blue/ViewController.swift
+++ Red&Blue/Red&Blue/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  MyFirstIOSApplication
+//  Red&Blue
 //
 //  Created by Sai Pratap Koppolu  on 18/05/16.
 //  Copyright © 2016 KSP. All rights reserved.
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var button: UIButton!
     
+    
+    @IBOutlet weak var red: UILabel!
+    @IBOutlet weak var blue: UILabel!
+
+    @IBOutlet weak var redbtn: UIButton!
+    
+    @IBOutlet weak var bluebtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,12 +27,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    @IBAction func button(sender: AnyObject) {
-        
-        label.hidden = false
-        button.hidden = true
+    @IBAction func redbtn(sender: AnyObject) {
+        red.hidden = true
+    }
+    @IBAction func bluebtn(sender: AnyObject) {
+        blue.hidden = true
     }
 
 }
